c61c7decb1ea15518e4ba289d23e29a8ff182a62
aman <amangangwani000@gmail.com>
2023-07-13 17:38:18 +0530

    done_both

---
+++ js/creative.js
@@ -0,0 +1,29 @@
+// nav bar toggling
+const btn = document.querySelector(".open_btn");
+const navBar = document.querySelector(".gym_nav_bar");
+const closeBtn = document.querySelector(".close-btn");
+
+btn.addEventListener("click", () => {
+  navBar.classList.toggle("add_display");
+});
+closeBtn.addEventListener("click", () => {
+  navBar.classList.toggle("add_display");
+});
+// contact us
+const contactBtn = document.querySelector(".link-5");
+const openContact = document.querySelector(".gym_contactUs");
+contactBtn.addEventListener("click", () => {
+  if (openContact.style.display === "none") openContact.style.display = "block";
+  else openContact.style.display = "none";
+});
+// contact button close functionality
+const btnn = document.querySelector(".close-bt");
+btnn.addEventListener("click", () => {
+  openContact.style.display = "none";
+});
+// form submission
+const textAfterSubmission = document.querySelector(".submit_confirm");
+function formSubmitter() {
+  textAfterSubmission.innerHTML = "*Your Form Has Been Submitted Succssfully!";
+  console.log(textAfterSubmission);
+}
